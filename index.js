7cec38c8ae4312b10272db8ff9deda49938a2956
kuldeep <kuldeep.rajput@300minds.com>
2022-06-22 10:09:53 +0530

    commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express =require('express')
+const express = require('express')
 const app = express()
 
-app.get('/api', (req, res)=>{
-    res.send('1111')
+app.get('/api', (req, res) => {
+    res.send('Hello')
 })
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log('app up')
 })
